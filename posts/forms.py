3a967834d742affaf58839d601eaf92c95d6ee0b
fanta95 <fantael95@gmail.com>
2023-05-25 15:37:44 +0900

    posts view작성

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Post
+from .models import Post,Comment
 from taggit.forms import TagField
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'category', 'image', 'content',)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
