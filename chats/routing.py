8d0d5ca26a852d5268fdc9cbb4ad5c91d8c2823d
mangji12 <dhckddls12@naver.com>
2023-06-14 09:09:25 +0900

    채팅기능 구현

--- chats/routing.py
+++ chats/routing.py
@@ -6,5 +6,5 @@ from .consumers import EchoConsumer, LiveblogConsumer
 websocket_urlpatterns =[
   path('ws/liveblog/', LiveblogConsumer.as_asgi()),
   path('ws/echo/', EchoConsumer.as_asgi()),
-  path('ws/chat/<str:room_name>/chat', consumers.ChatConsumer.as_asgi())
+  path('ws/chats/<str:room_name>/chats', consumers.ChatConsumer.as_asgi())
 ]
--- chats/urls.py
+++ chats/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('liveblog/',views.liveblog_index),
     path('liveblog/posts/<int:post_id>/',views.post_partial,name='post_partial'),
-    path('<str:room_name>/chat/',views.room_chat,name='room_chat'),
+    path('<str:room_name>/chats/',views.room_chat,name='room_chat'),
     path('echo/', views.echo_page),
 ]
